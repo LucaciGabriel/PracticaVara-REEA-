4d86f5682f67e7e1630655cc60bbb1760908bd4e
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2023-07-20 03:42:47 -0700

    Add files via upload

---
+++ ProiectLucaciGabriel/Js/Navbarchange.js
@@ -0,0 +1,9 @@
+const nav = document.getElementById('navbar');
+window.onscroll = function () { 
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ) {
+        navbar.className="nav-solid";
+    } 
+    else {
+        navbar.className="nav-transparent";
+    }
+};
