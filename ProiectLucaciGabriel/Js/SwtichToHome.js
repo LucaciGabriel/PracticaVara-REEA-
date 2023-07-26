1e582367a2c2c58c427d50b72946a223746ca5df
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2023-07-26 03:10:48 -0700

    Add files via upload

--- ProiectLucaciGabriel/Js/SwtichToHome.js
+++ ProiectLucaciGabriel/Js/SwtichToHome.js
@@ -1,13 +1,20 @@
 function switchToHome() {
-    var currentdate = new Date();
-    var age = currentdate.getFullYear() - document.getElementById("yearinput").value;
-    var m = currentdate.getMonth() - document.getElementById("monthinput").value+1;
-    if (m < 0 || (m === 0 && currentdate.getDate() < document.getElementById("dayinput").value)) {
-        age--;
-    }
-    if(age>=18)window.location.href="ViennaDistributionHomePage.html";
-    else 
+    if((document.getElementById("dayinput").value==='' || document.getElementById("dayinput").value===0||(document.getElementById("dayinput").value>31)) ||
+       (document.getElementById("monthinput").value==='' || document.getElementById("monthinput").value===0||(document.getElementById("monthinput").value>12)) ||
+       (document.getElementById("yearinput").value==='' || document.getElementById("yearinput").value<1920||(document.getElementById("dayinput").value>2023)))
+       document.getElementById("alertbox").style.display= "block";
+    else
     {
-        document.getElementById("alertbox").style.display= "block";
+        var currentdate = new Date();
+        var age = currentdate.getFullYear() - document.getElementById("yearinput").value;
+        var m = currentdate.getMonth() - document.getElementById("monthinput").value + 1;
+        if (m < 0 || (m === 0 && currentdate.getDate() < document.getElementById("dayinput").value)) {
+            age--;
+        }
+        if(age>=18)window.location.href="ViennaDistributionHomePage.html";
+        else 
+        {
+            document.getElementById("alertbox").style.display= "block";
+        }
     }
 }
